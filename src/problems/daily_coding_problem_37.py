ed291e702220d75751dfd0ddebbd29ce69311ff2
Łukasz Ściga <scigalukasz@gmail.com>
2022-01-30 19:52:54 +0100

    Add bottom up for 37

--- src/problems/daily_coding_problem_37.py
+++ src/problems/daily_coding_problem_37.py
@@ -8,9 +8,6 @@ def do_top_down(nums: list[int], results: list[list[int]]):
         if nums not in results:
             results.append(nums)
         do_top_down(new_nums, results)
-
-    # todo cache?
-    # todo bottom up
     return results
 
 
@@ -26,3 +23,18 @@ def top_down(nums: list[int]):
     do_top_down(nums, results)
     return results
 
+
+def bottom_up(nums: list[int]):
+    """
+    0 - {}                                  = {}
+    1 - f(0) + for x in f(0): x + {a}       = {}, {1}
+    2 - f(1) + for x in f(1): x + {b}       = {}, {1}, {2}, {1, 2}
+    3 - f(2) + for x in f(2): x + {c}       = {}, {1}, {2}, {1, 2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}
+    """
+    results = [[]]
+    for i, num in enumerate(nums):
+        new_results = []
+        for prev_results in results:
+            new_results.append(prev_results + [num])
+        results = results + new_results
+    return results
--- test/test_daily_coding_problem_37.py
+++ test/test_daily_coding_problem_37.py
@@ -1,9 +1,17 @@
 import pytest
-from problems.daily_coding_problem_37 import top_down
+from problems.daily_coding_problem_37 import top_down, bottom_up
+
 
 @pytest.mark.parametrize("nums,expected", [
     ([1, 2], [[], [1], [2], [1, 2]]),
     ([1, 2, 3], [[], [1], [2], [3], [1, 2], [2, 3], [1, 3], [1, 2, 3]]),
 ])
-def test(nums, expected):
+def test_bottom_up(nums, expected):
+    assert sorted(bottom_up(nums)) == sorted(expected)
+
+@pytest.mark.parametrize("nums,expected", [
+    ([1, 2], [[], [1], [2], [1, 2]]),
+    ([1, 2, 3], [[], [1], [2], [3], [1, 2], [2, 3], [1, 3], [1, 2, 3]]),
+])
+def test_top_down(nums, expected):
     assert sorted(top_down(nums)) == sorted(expected)
